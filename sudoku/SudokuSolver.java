9b3c7c24a3d68b0096c73db12cddab7aad08f7df
Terri Paik <terri@auderenow.org>
2023-11-09 09:00:12 -0800

    Working logic for easy, medium, hard

--- sudoku/SudokuSolver.java
+++ sudoku/SudokuSolver.java
@@ -8,11 +8,16 @@ import java.util.*;
 public class SudokuSolver {
     public static final int BLANK = -1;
     static int[][] grid = new int[9][9];
-    static ArrayList<Integer>[][] possibles;
+    static ArrayList<Integer>[][] possiblesGrid = new ArrayList[9][9];
     static {
+        for (int i = 0; i < 0; i++) {
+            for (int j = 0; j < 9; j++) {
+                possiblesGrid[i][j] = new ArrayList<Integer>();
+            }
+        }
     }
 
-    public static ArrayList<Integer> SUDOKU_SET = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    public static ArrayList<Integer> POSSIBLES_SET = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
     static void printGrid() {
         for (int i = 0; i < 9; i++) {
@@ -63,71 +68,19 @@ public class SudokuSolver {
         }
     }
 
-    static int solveByRow(int blank_i, int blank_j) {
-        ArrayList<Integer> sudokuSet = (ArrayList<Integer>) SUDOKU_SET.clone();
-        for (int j = 0; j < 9; j++) {
-            if (j != blank_j) {
-                sudokuSet.remove(Integer.valueOf(grid[blank_i][j] - 1));
-            }
-        }
-        if (sudokuSet.size() == 1) {
-            int onlyChoice = sudokuSet.get(0);
-            grid[blank_i][blank_j] = onlyChoice;
-            return onlyChoice;
-        } else {
-            return BLANK;
-        }
-    }
-
-    static int solveByCol(int blank_i, int blank_j) {
-        ArrayList<Integer> sudokuSet = (ArrayList<Integer>) SUDOKU_SET.clone();
-        for (int i = 0; i < 9; i++) {
-            if (i != blank_i) {
-                sudokuSet.remove(Integer.valueOf(grid[i][blank_j]));
-            }
-        }
-        if (sudokuSet.size() == 1) {
-            int onlyChoice = sudokuSet.get(0);
-            grid[blank_i][blank_j] = onlyChoice;
-            return onlyChoice;
-        } else {
-            return BLANK;
-        }
-    }
-
-    static int solveBySquare(int blank_i, int blank_j) {
-        ArrayList<Integer> sudokuSet = (ArrayList<Integer>) SUDOKU_SET.clone();
-        int squareStartX = Math.floorDiv(blank_i, 3) * 3;
-        int squareStartY = Math.floorDiv(blank_j, 3) * 3;
-        for (int i = squareStartX; i < squareStartX + 3; i++) {
-            for (int j = squareStartY; j < squareStartY + 3; j++) {
-                if (i != blank_i || j != blank_j) {
-                    sudokuSet.remove(Integer.valueOf(grid[i][j]));
-                }
-            }
-        }
-        if (sudokuSet.size() == 1) {
-            int onlyChoice = sudokuSet.get(0);
-            grid[blank_i][blank_j] = onlyChoice;
-            return onlyChoice;
-        } else {
-            return BLANK;
-        }
-    }
-
-    static int solveBlankIfPossible(int blank_i, int blank_j) {
-        System.out.println("solveBlankIfPossible " + blank_i + ", " + blank_j);
-        ArrayList<Integer> sudokuSet = (ArrayList<Integer>) SUDOKU_SET.clone();
+    static void computePossibles(int blank_i, int blank_j) {
+        System.out.println("computePossibles " + blank_i + ", " + blank_j);
+        ArrayList<Integer> possibles = (ArrayList<Integer>) POSSIBLES_SET.clone();
         for (int i = 0; i < 9; i++) {
             if (grid[i][blank_j] != BLANK) {
-                sudokuSet.remove(Integer.valueOf(grid[i][blank_j]));
-                System.out.println("removed " + grid[i][blank_j]);
+                possibles.remove(Integer.valueOf(grid[i][blank_j]));
+                System.out.print(" " + grid[i][blank_j]);
             }
         }
         for (int j = 0; j < 9; j++) {
             if (grid[blank_i][j] != BLANK) {
-                sudokuSet.remove(Integer.valueOf(grid[blank_i][j]));
-                System.out.println("removed " + grid[blank_i][j]);
+                possibles.remove(Integer.valueOf(grid[blank_i][j]));
+                System.out.print(" " + grid[blank_i][j]);
             }
         }
         int squareStartX = Math.floorDiv(blank_i, 3) * 3;
@@ -135,28 +88,125 @@ public class SudokuSolver {
         for (int i = squareStartX; i < squareStartX + 3; i++) {
             for (int j = squareStartY; j < squareStartY + 3; j++) {
                 if (grid[i][j] != BLANK) {
-                    sudokuSet.remove(Integer.valueOf(grid[i][j]));
-                    System.out.println("removed " + grid[i][j]);
+                    possibles.remove(Integer.valueOf(grid[i][j]));
+                    System.out.print(" " + grid[i][j]);
                 }
             }
         }
-        if (sudokuSet.size() == 1) {
-            int onlyChoice = sudokuSet.get(0);
-            grid[blank_i][blank_j] = onlyChoice;
+    }
+
+    static void markAnswer(int blank_i, int blank_j, int answer) {
+        grid[blank_i][blank_j] = answer;
+        possiblesGrid[blank_i][blank_j] = null;
+        // remove onlyChoice from possibles of this blank's row or col
+        for (int i = 0; i < 9; i++) {
+            if (i != blank_i && possiblesGrid[i][blank_j] != null) {
+                possiblesGrid[i][blank_j].remove(Integer.valueOf(answer));
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            if (j != blank_j && possiblesGrid[blank_i][j] != null) {
+                possiblesGrid[blank_i][j].remove(Integer.valueOf(answer));
+            }
+        }
+        int squareStartX = Math.floorDiv(blank_i, 3) * 3;
+        int squareStartY = Math.floorDiv(blank_j, 3) * 3;
+        for (int i = squareStartX; i < squareStartX + 3; i++) {
+            for (int j = squareStartY; j < squareStartY + 3; j++) {
+                if ((i != blank_i || j != blank_j) && possiblesGrid[i][j] != null) {
+                    possiblesGrid[i][j].remove(Integer.valueOf(answer));
+                }
+            }
+        }
+    }
+
+    static int seeIfOnlyOnePossible(int blank_i, int blank_j) {
+        System.out.println("solveBlankIfPossible " + blank_i + ", " + blank_j);
+        ArrayList<Integer> possibles = (ArrayList<Integer>) POSSIBLES_SET.clone();
+        for (int i = 0; i < 9; i++) {
+            if (grid[i][blank_j] != BLANK) {
+                possibles.remove(Integer.valueOf(grid[i][blank_j]));
+                System.out.print(" c" + grid[i][blank_j]);
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            if (grid[blank_i][j] != BLANK) {
+                possibles.remove(Integer.valueOf(grid[blank_i][j]));
+                System.out.print(" r" + grid[blank_i][j]);
+            }
+        }
+        int squareStartX = Math.floorDiv(blank_i, 3) * 3;
+        int squareStartY = Math.floorDiv(blank_j, 3) * 3;
+        for (int i = squareStartX; i < squareStartX + 3; i++) {
+            for (int j = squareStartY; j < squareStartY + 3; j++) {
+                if (grid[i][j] != BLANK) {
+                    possibles.remove(Integer.valueOf(grid[i][j]));
+                    System.out.print(" s" + grid[i][j]);
+                }
+            }
+        }
+        if (possibles.size() == 1) {
+            int onlyChoice = possibles.get(0);
+            markAnswer(blank_i, blank_j, onlyChoice);
             return onlyChoice;
         } else {
-            System.out.println("sudokuSet " + sudokuSet);
+            possiblesGrid[blank_i][blank_j] = possibles;
+            System.out.println("possibles " + possibles);
             return BLANK;
         }
-        // if (solveByRow(blank_i, blank_j) == BLANK) {
-        // if (solveByCol(blank_i, blank_j) == BLANK) {
-        // if (solveBySquare(blank_i, blank_j) == BLANK) {
-        // System.out.println("didn't solve " + blank_i + ", " + blank_j);
-        // return BLANK;
-        // }
-        // }
-        // }
-        // return grid[blank_i][blank_j];
+    }
+
+    static int seeIfAPossibleCantGoAnywhereElse(int blank_i, int blank_j) {
+        for (int x : possiblesGrid[blank_i][blank_j]) {
+            boolean thisPossibleExistsElsewhere = false;
+            for (int i = 0; i < 9; i++) {
+                if (i != blank_i) {
+                    ArrayList<Integer> possiblesForOtherSquare = possiblesGrid[i][blank_j];
+                    if (possiblesForOtherSquare != null && possiblesForOtherSquare.contains(x)) {
+                        thisPossibleExistsElsewhere = true;
+                        break;
+                    }
+                }
+            }
+            if (!thisPossibleExistsElsewhere) {
+                markAnswer(blank_i, blank_j, x);
+                return x;
+            }
+            thisPossibleExistsElsewhere = false;
+            for (int j = 0; j < 9; j++) {
+                if (j != blank_j) {
+                    ArrayList<Integer> possiblesForOtherSquare = possiblesGrid[blank_i][j];
+                    if (possiblesForOtherSquare != null && possiblesForOtherSquare.contains(x)) {
+                        thisPossibleExistsElsewhere = true;
+                        break;
+                    }
+                }
+            }
+            if (!thisPossibleExistsElsewhere) {
+                markAnswer(blank_i, blank_j, x);
+                return x;
+            }
+
+            int squareStartX = Math.floorDiv(blank_i, 3) * 3;
+            int squareStartY = Math.floorDiv(blank_j, 3) * 3;
+            thisPossibleExistsElsewhere = false;
+            for (int i = squareStartX; i < squareStartX + 3; i++) {
+                for (int j = squareStartY; j < squareStartY + 3; j++) {
+                    if (i != blank_i || j != blank_j) {
+                        ArrayList<Integer> possiblesForOtherSquare = possiblesGrid[i][j];
+                        if (possiblesForOtherSquare != null && possiblesForOtherSquare.contains(x)) {
+                            thisPossibleExistsElsewhere = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!thisPossibleExistsElsewhere) {
+                markAnswer(blank_i, blank_j, x);
+                return x;
+            }
+        }
+        return BLANK;
     }
 
     static void testEachBlank() {
@@ -164,10 +214,21 @@ public class SudokuSolver {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (grid[i][j] == BLANK) {
-                    int answer = solveBlankIfPossible(i, j);
+                    int answer = seeIfOnlyOnePossible(i, j);
                     if (answer != BLANK) {
                         dirty = true;
-                        System.out.println("Solved " + i + ", " + j + " = " + answer);
+                        System.out.println("\n*** SOLVED (only possible) " + i + ", " + j + " = " + answer);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] == BLANK) {
+                    int answer = seeIfAPossibleCantGoAnywhereElse(i, j);
+                    if (answer != BLANK) {
+                        dirty = true;
+                        System.out.println("\n*** SOLVED (only place for possible) " + i + ", " + j + " = " + answer);
                     }
                 }
             }
@@ -188,5 +249,6 @@ public class SudokuSolver {
         while (!isSolved()) {
             testEachBlank();
         }
+        printGrid();
     }
 }
